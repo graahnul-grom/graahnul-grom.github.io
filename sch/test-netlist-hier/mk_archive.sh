184faff8d8768cea6f382568af3d81332e92785a
dmn <graahnul.grom@gmail.com>
2019-12-03 13:56:14 +0300

    sch/test-netlist-hier: rename dir; add mk_archive

---
+++ sch/test-netlist-hier/mk_archive.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+out=sch-test-netlist-hier.tar.gz
+# out=sch-test-netlist-hier_project-archive.tar.gz
+exe=$lepton/bin.master/bin/lepton-archive
+
+export GUILE_AUTO_COMPILE=0
+$exe -v -o $out schematic-main.sch geda.conf
+
