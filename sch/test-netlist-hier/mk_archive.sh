408c5f9c19275c056906f1e26449961d2eff1f78
dmn <graahnul.grom@gmail.com>
2020-01-31 18:21:25 +0300

    sch/test-netlist-hier: update project archive

--- sch/test-netlist-hier/mk_archive.sh
+++ sch/test-netlist-hier/mk_archive.sh
@@ -5,5 +5,5 @@ out=sch-test-netlist-hier.tar.gz
 exe=$lepton/bin.master/bin/lepton-archive
 
 export GUILE_AUTO_COMPILE=0
-$exe -v -o $out schematic-main.sch geda.conf
+$exe -v -o $out main.sch geda.conf
 
