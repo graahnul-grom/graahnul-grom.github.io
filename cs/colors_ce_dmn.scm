14e30a6b39fc554976771156d3395d9d34e652a9
dmn <graahnul.grom@gmail.com>
2020-05-08 21:09:25 +0300

    cs: update colors_ce_dmn color scheme

--- cs/colors_ce_dmn.scm
+++ cs/colors_ce_dmn.scm
@@ -1,24 +1,25 @@
 ; file: colors_ce_dmn.scm
-; copyright (c) 2019 dmn <graahnul.grom@gmail.com>
+; copyright (c) 2019-2020 dmn <graahnul.grom@gmail.com>
 ; license: GPLv2+
 ;
 ; lepton-schematic color scheme
 ;   created with [View->Color Scheme Editor...]
-; usage: add the following to gschemrc configuration file:
+; usage: add the following to your gschemrc configuration file:
 ;   ( primitive-load "/path/to/colors_ce_dmn.scm" )
 ;
 
 (display-color-map '(
   (background           "#002b36ff")
   (pin                  "#c18f5fff")
-  (net-endpoint         "#ef5121ff")
+  (net-endpoint         "#00B330ff")
   (graphic              "#cc9d47ff")
   (net                  "#bebebeff")
   (attribute            "#abababff")
   (logic-bubble         "#ef653bff")
   (dots-grid            "#006680ff")
   (detached-attribute   "#ef5121ff")
-  (text                 "#799aa9ff")
+  (text                 "#00A2CC")
+  ; (text                 "#799aa9ff") ; default darkbg cs text color
   (bus                  "#26c3d9ff")
   (select               "#c7ec10ff")
   (bounding-box         "#76e665ff")
@@ -38,7 +39,7 @@
 (display-outline-color-map '(
   (background           "#002b36af")
   (pin                  "#c18f5faf")
-  (net-endpoint         "#ef5121af")
+  (net-endpoint         "#00B330af")
   (graphic              "#cc9d47af")
   (net                  "#bebebeaf")
   (attribute            "#abababaf")
