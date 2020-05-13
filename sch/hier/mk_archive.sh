86dd42c046e83e43c1ce8b89e0418dcd5036ffff
dmn <graahnul.grom@gmail.com>
2020-05-13 20:15:45 +0300

    sch/hier: rename tar.gz archive; rm old script

--- sch/hier/mk_archive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-out=sch-test-netlist-hier.tar.gz
-# out=sch-test-netlist-hier_project-archive.tar.gz
-exe=$lepton/bin.master/bin/lepton-archive
-
-export GUILE_AUTO_COMPILE=0
-$exe -v -o $out main.sch geda.conf
-
