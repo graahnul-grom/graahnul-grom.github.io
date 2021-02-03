4c9de66368c3dc4e4f933cfc48fe0c9df184779b
dmn <graahnul.grom@gmail.com>
2021-02-03 14:15:27 +0300

    cs: add "zelenuha" dmn color scheme files

---
+++ cs/cs/colors_zelenuha_print_dmn.scm
@@ -0,0 +1,38 @@
+; file: colors_zelenuha_print_dmn.scm
+; type: lepton-schematic color scheme
+; copyright (c) 2019-2021 dmn <graahnul.grom@gmail.com>
+; license: GPLv2+
+;
+; Color scheme created with View->Color Scheme Editor.
+; Usage: add the following to your gschemrc configuration file:
+;   ( primitive-load "/path/to/colors_zelenuha_print_dmn.scm" )
+;
+
+(print-color-map '(
+  (background           "#002b36ff")
+  (pin                  "#c18f5fff")
+  (net-endpoint         "#00B330ff")
+  (graphic              "#cc9d47ff")
+  (net                  "#bebebeff")
+  (attribute            "#abababff")
+  (logic-bubble         "#ef653bff")
+  (dots-grid            "#006680ff")
+  (detached-attribute   "#ef5121ff")
+  (text                 "#00A2CC")
+  ; (text                 "#799aa9ff") ; default darkbg cs text color
+  (bus                  "#26c3d9ff")
+  (select               "#c7ec10ff")
+  (bounding-box         "#76e665ff")
+  (zoom-box             "#ff94e9ff")
+  (stroke               "#ffe5b5ff")
+  (lock                 "#919191ff")
+  (output-background    "#002b36ff")
+  (freestyle1           "#97a547ff")
+  (freestyle2           "#c18f5fff")
+  (freestyle3           "#ca5d31ff")
+  (freestyle4           "#76e665ff")
+  (junction             "#ffffffff")
+  (mesh-grid-major      "#003947ff")
+  (mesh-grid-minor      "#003240ff")
+))
+
